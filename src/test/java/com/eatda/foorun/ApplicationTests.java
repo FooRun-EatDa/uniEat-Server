102cff6707047ac042548b1a0684479e30e0f567
junyharang <junyharang8592@gmail.com>
2022-01-05 15:35:18 +0900

    기능추가🔧 #1 초기 구성

---
+++ src/test/java/com/eatda/foorun/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatda.foorun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
