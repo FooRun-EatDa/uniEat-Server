2835f6f9072c3c50424e7f15bc24f973929b5422
chaehoon <youspend@gmail.com>
2022-02-06 15:01:02 +0900

    feat : swagger-ui 설정

--- src/main/java/com/foorun/unieat/controller/PostController.java
+++ src/main/java/com/foorun/unieat/controller/PostController.java
@@ -1,10 +1,14 @@
 package com.foorun.unieat.controller;
 
+import com.foorun.unieat.constant.SwaggerApiInfo;
 import com.foorun.unieat.domain.common.paging.Paging;
 import com.foorun.unieat.domain.post.dto.Post;
 import com.foorun.unieat.domain.post.dto.PostList;
 import com.foorun.unieat.service.post.PostListService;
 import com.foorun.unieat.service.post.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,28 +17,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
+@Api(tags = SwaggerApiInfo.POST)
 public class PostController {
     private final PostService postService;
     private final PostListService postListService;
 
+    @ApiOperation(value = SwaggerApiInfo.GET_POST_LIST)
     @GetMapping
     public List<PostList> get(@ModelAttribute Paging paging) {
         System.out.println("pageRequest --> " + paging);
         return postListService.fetch(paging);
     }
 
+    @ApiOperation(value = SwaggerApiInfo.GET_POST)
     @GetMapping("/{id}")
-    public Post get(@PathVariable("id") long id) {
+    public Post get(@ApiParam(required = true, name = "게시글 고유 ID") @PathVariable("id") long id) {
         return postService.fetch(id);
     }
 
+    @ApiOperation(value = SwaggerApiInfo.POST_POST)
     @PostMapping
     public void post(@RequestBody Post post) {
         postService.save(post);
     }
 
+    @ApiOperation(value = SwaggerApiInfo.DELETE_POST)
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") long id) {
+    public void delete(@ApiParam(required = true, name = "게시글 고유 ID") @PathVariable("id") long id) {
         postService.remove(id);
     }
 }
--- src/main/java/com/foorun/unieat/domain/common/paging/Paging.java
+++ src/main/java/com/foorun/unieat/domain/common/paging/Paging.java
@@ -14,7 +14,7 @@ public class Paging extends PageRequest {
      * Creates a new {@link PageRequest} with sort parameters applied.
      *
      * @param page zero-based page index, must not be negative.
-     * @param size the size of the page to be returned, must be greater than 0.
+     * @param offset the size of the page to be returned, must be greater than 0.
      * @param sort must not be {@literal null}, use {@link Sort#unsorted()} instead.
      */
     protected Paging(int page, int offset, Sort sort) {
