382926cbc05486e7118d5738ef4d0ed60e07f671
chaehoon <youspend@gmail.com>
2022-02-06 15:56:39 +0900

    feat : 게시글 API 응답 객체 ResponseEntity<ApiResponse<T>> 형태로 Wrapping

--- src/main/java/com/foorun/unieat/controller/PostController.java
+++ src/main/java/com/foorun/unieat/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.foorun.unieat.controller;
 
 import com.foorun.unieat.constant.SwaggerApiInfo;
+import com.foorun.unieat.domain.common.api.ApiResponse;
 import com.foorun.unieat.domain.common.paging.Paging;
 import com.foorun.unieat.domain.post.dto.Post;
 import com.foorun.unieat.domain.post.dto.PostList;
@@ -10,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,26 +26,30 @@ public class PostController {
 
     @ApiOperation(value = SwaggerApiInfo.GET_POST_LIST)
     @GetMapping
-    public List<PostList> get(@ModelAttribute Paging paging) {
-        System.out.println("pageRequest --> " + paging);
-        return postListService.fetch(paging);
+    public ResponseEntity<ApiResponse<List<PostList>>> get(@ModelAttribute Paging paging) {
+        return ResponseEntity.ok(
+                ApiResponse.valueOf(postListService.fetch(paging)));
     }
 
     @ApiOperation(value = SwaggerApiInfo.GET_POST)
     @GetMapping("/{id}")
-    public Post get(@ApiParam(required = true, name = "게시글 고유 ID") @PathVariable("id") long id) {
-        return postService.fetch(id);
+    public ResponseEntity<ApiResponse<Post>> get(
+            @ApiParam(required = true, name = "게시글 고유 ID") @PathVariable("id") long id) {
+        return ResponseEntity.ok(
+                ApiResponse.valueOf(postService.fetch(id)));
     }
 
     @ApiOperation(value = SwaggerApiInfo.POST_POST)
     @PostMapping
-    public void post(@RequestBody Post post) {
-        postService.save(post);
+    public ResponseEntity<ApiResponse<Void>> post(@RequestBody Post post) {
+        return ResponseEntity.ok(
+                ApiResponse.execute(() -> postService.save(post)));
     }
 
     @ApiOperation(value = SwaggerApiInfo.DELETE_POST)
     @DeleteMapping("/{id}")
-    public void delete(@ApiParam(required = true, name = "게시글 고유 ID") @PathVariable("id") long id) {
-        postService.remove(id);
+    public ResponseEntity<ApiResponse<Void>> delete(@ApiParam(required = true, name = "게시글 고유 ID") @PathVariable("id") long id) {
+        return ResponseEntity.ok(
+                ApiResponse.execute(() -> postService.remove(id)));
     }
 }
