19ffdf9d2890f3fdf4f2cb7f83a9b6f0dffc671b
chaehoon <youspend@gmail.com>
2022-02-09 15:49:05 +0900

    feat : ObjectMapper Bean 작성

--- src/main/java/com/foorun/unieat/config/ObjectMapperConfiguration.java
+++ src/main/java/com/foorun/unieat/config/ObjectMapperConfiguration.java
@@ -1,2 +1,16 @@
-package com.foorun.unieat.config;public class ObjectMapperConfiguration {
+package com.foorun.unieat.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foorun.unieat.config.factory.ObjectMapperFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ObjectMapperConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return ObjectMapperFactory.getInstance();
+    }
 }
