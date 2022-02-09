d4cff84d169cfdf4c3a89d2fe31cd7f5335c323a
chaehoon <youspend@gmail.com>
2022-02-09 15:50:03 +0900

    fix : ObjectMapper JavaTimeModule 추가
    - LocalDateTime / LocalDate 형식 문자열로 변환

--- src/main/java/com/foorun/unieat/util/JsonUtil.java
+++ src/main/java/com/foorun/unieat/util/JsonUtil.java
@@ -2,8 +2,6 @@ package com.foorun.unieat.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.foorun.unieat.config.factory.ObjectMapperFactory;
 
 /**
@@ -14,8 +12,6 @@ public final class JsonUtil {
 
     static {
         objectMapper = ObjectMapperFactory.getInstance();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     public static String asJson(Object object) {
