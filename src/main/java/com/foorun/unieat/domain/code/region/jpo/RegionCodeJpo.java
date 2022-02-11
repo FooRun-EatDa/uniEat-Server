2cebd864e658ec799f2fdd2c48fa8507c86a7f33
chaehoon <youspend@gmail.com>
2022-02-11 13:49:50 +0900

    feat : 지역코드(RegionCodeJpo), 좌표(CoordinateJpo) Entity 작성

---
+++ src/main/java/com/foorun/unieat/domain/code/region/jpo/RegionCodeJpo.java
@@ -0,0 +1,53 @@
+package com.foorun.unieat.domain.code.region.jpo;
+
+import com.foorun.unieat.domain.code.region.jpo.embed.CoordinateJpo;
+import lombok.*;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+@Table(name = "region_code")
+public class RegionCodeJpo {
+    @Id
+    private Long id;
+
+    /**
+     * 광역시/특별시/도 코드 --> 2자리 정수
+     */
+    private int sido;
+
+    /**
+     * 시/군/구 코드 --> 3자리 정수
+     */
+    private int sigungu;
+
+    /**
+     * 읍/면/동 코드 --> 5자리 정수
+     */
+    private int umd;
+
+    /**
+     * 행정 구역 명(주소)
+     */
+    private String name;
+
+    /**
+     * 전체 주소
+     */
+    private String fullName;
+
+    /**
+     * 좌표
+     */
+    @Embedded
+    private CoordinateJpo coordinate;
+}
