1b3f039138884f0797f94a5b8ad0e2c7da47d894
chaehoon <youspend@gmail.com>
2022-02-18 17:25:05 +0900

    feat : 리소스 유효기간 만료 예외 작성

---
+++ src/main/java/com/foorun/unieat/exception/notfound/UniEatResourceExpiryException.java
@@ -0,0 +1,7 @@
+package com.foorun.unieat.exception.notfound;
+
+import com.foorun.unieat.exception.UniEatNotFoundException;
+
+public class UniEatResourceExpiryException extends UniEatNotFoundException {
+
+}
