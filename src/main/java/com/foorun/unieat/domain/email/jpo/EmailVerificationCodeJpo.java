620412494e4e0455b80e7a68699e80ebd5c92f0f
chaehoon <youspend@gmail.com>
2022-02-18 16:19:21 +0900

    feat : 이메일 인증 코드 발송 유효시간 관리 테이블 Entity 작성

---
+++ src/main/java/com/foorun/unieat/domain/email/jpo/EmailVerificationCodeJpo.java
@@ -0,0 +1,30 @@
+package com.foorun.unieat.domain.email.jpo;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+@Table(name = "email_verification_code")
+@EntityListeners(AuditingEntityListener.class)
+public class EmailVerificationCodeJpo {
+    @Id
+    @Column(columnDefinition = "이메일")
+    private String email;
+
+    @Column(columnDefinition = "발급된 인증 코드")
+    private String code;
+
+    @CreatedDate
+    @Column(name = "created_at", columnDefinition = "발급일시")
+    private LocalDateTime createdAt;
+}
