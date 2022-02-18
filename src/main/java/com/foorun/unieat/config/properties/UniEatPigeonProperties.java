744354b9e8af7a4a3780c23d3bc42962d2c4d127
chaehoon <youspend@gmail.com>
2022-02-18 17:51:01 +0900

    fix : UniEatPigeonProperties Enable Entrypoint class로 이동

--- src/main/java/com/foorun/unieat/config/properties/UniEatPigeonProperties.java
+++ src/main/java/com/foorun/unieat/config/properties/UniEatPigeonProperties.java
@@ -4,13 +4,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @Getter
 @ConfigurationProperties("pigeon")
 @ConstructorBinding
 @RequiredArgsConstructor
-@EnableConfigurationProperties({ UniEatPigeonProperties.class })
 public class UniEatPigeonProperties {
     private final String baseUrl;
     private final String contextUri;
