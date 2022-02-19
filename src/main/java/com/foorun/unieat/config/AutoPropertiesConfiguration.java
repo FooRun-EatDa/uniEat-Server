38f77f794ea0195d3526e4256c21693826eafb07
chaehoon <youspend@gmail.com>
2022-02-19 17:33:33 +0900

    remove : AutoPropertiesConfiguration

--- src/main/java/com/foorun/unieat/config/AutoPropertiesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.foorun.unieat.config;
-
-import com.foorun.unieat.config.properties.UniEatPigeonProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AutoPropertiesConfiguration {
-}
