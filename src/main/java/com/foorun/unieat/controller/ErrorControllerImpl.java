33681f38e2d6c72401b66f3d0662ce6a9906d24e
chaehoon <youspend@gmail.com>
2022-02-25 15:18:00 +0900

    fix : ErrorController Swagger 제외

--- src/main/java/com/foorun/unieat/controller/ErrorControllerImpl.java
+++ src/main/java/com/foorun/unieat/controller/ErrorControllerImpl.java
@@ -2,20 +2,20 @@ package com.foorun.unieat.controller;
 
 import com.foorun.unieat.constant.ResponseCode;
 import com.foorun.unieat.domain.common.api.ApiResponse;
-import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/error")
 @RequiredArgsConstructor
-@Api(hidden = true)
+@ApiIgnore
 public class ErrorControllerImpl implements ErrorController {
     private final HttpServletResponse response;
 
