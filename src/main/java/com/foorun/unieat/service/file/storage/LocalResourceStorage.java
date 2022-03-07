efe99d01a3984ee573a89d4b355c41e798b8704a
chaehoon <youspend@gmail.com>
2022-03-07 13:52:45 +0900

    feat : 파일 업로드 API 완료

---
+++ src/main/java/com/foorun/unieat/service/file/storage/LocalResourceStorage.java
@@ -0,0 +1,16 @@
+package com.foorun.unieat.service.file.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@ConditionalOnMissingBean(AmazonS3ResourceStorage.class)
+@RequiredArgsConstructor
+public class LocalResourceStorage implements ResourceStorage {
+    @Override
+    public void store(String fullPath, MultipartFile multipartFile) {
+
+    }
+}
