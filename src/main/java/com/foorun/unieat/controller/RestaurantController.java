757d2a7a7ce228264e7abe04d917d09346855c42
philos1234 <ohhyun1948@gmail.com>
2022-06-01 17:04:48 +0900

    hotFix : Restaurant Controller 수정

--- src/main/java/com/foorun/unieat/controller/RestaurantController.java
+++ src/main/java/com/foorun/unieat/controller/RestaurantController.java
@@ -79,7 +79,6 @@ public class RestaurantController {
         );
     }
 
-    @ApiImplicitParam(name = "id", value = "유저 id", type="Long")
     @GetMapping(value = "/searchLog")
     public ResponseEntity<ApiResponse<List<SearchLog>>> getSearchLog(
             @AuthenticationPrincipal MemberUserDetails memberUserDetails) {
@@ -96,7 +95,7 @@ public class RestaurantController {
      */
     @ApiOperation(value =  SwaggerApiInfo.GET_NEAREST_STORE, notes = "사용자 현재 위치에 따른 주변 맛집 검색")
     @PostMapping(value = "/near")
-    public ResponseEntity<ApiResponse<List<RestaurantSimple>>> getNearestRestaurant(@ModelAttribute MemberLocation memberLocation)
+    public ResponseEntity<ApiResponse<List<RestaurantSimple>>g> getNearestRestaurant(@ModelAttribute MemberLocation memberLocation)
     {
         return ResponseEntity.ok(
                 ApiResponse.valueOf(restaurantService.fetchNearest(memberLocation))
