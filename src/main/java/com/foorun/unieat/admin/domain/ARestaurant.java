0c272fc294d171defd21f95991458e5ea73f7ed4
chaehun <chaehun@uniess.co.kr>
2022-08-07 16:10:16 +0900

    feat : ARestaurant 위경도 타입 변경

--- src/main/java/com/foorun/unieat/admin/domain/ARestaurant.java
+++ src/main/java/com/foorun/unieat/admin/domain/ARestaurant.java
@@ -33,8 +33,8 @@ public class ARestaurant implements JsonSerializable {
     private String content;
     private Integer category;
     private String address;
-    private String longitude;
-    private String latitude;
+    private Double longitude;
+    private Double latitude;
     private String phoneNumber;
     private String operationTime;
     private int price;
